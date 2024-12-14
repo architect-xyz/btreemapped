081c73d7ab3bbc849e22ab0a842f239b06d648ae
Andrew Lee <126016409+andrew-lee-architect@users.noreply.github.com>
2024-12-14 07:42:28 +0700

    _

--- btreemapped/src/sink.rs
+++ btreemapped/src/sink.rs
@@ -87,6 +87,7 @@ impl<T: BTreeMapped<N>, const N: usize> ErasedBTreeMapSink for BTreeMapSink<T, N
     }
 
     fn commit(&mut self, commit_lsn: PgLsn) {
+        self.committed_lsn = commit_lsn;
         let mut updates = vec![];
         if let Some((seqid, seqno)) = if self.txn_clog.is_empty() {
             None
@@ -108,7 +109,6 @@ impl<T: BTreeMapped<N>, const N: usize> ErasedBTreeMapSink for BTreeMapSink<T, N
             replica.seqno += 1;
             Some((replica.seqid, replica.seqno))
         } {
-            self.committed_lsn = commit_lsn;
             self.replica.changed.notify_waiters();
             if let Err(_) = self.replica.updates.send(Arc::new(BTreeUpdate {
                 seqid,
