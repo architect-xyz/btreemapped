107f6f94f31850e2c6c2eeacbe8dda866afad332
michael-rees-architect <m.rees@architect.co>
2025-10-04 11:59:39 -0500

    Add support for encoding/decoding PgJson with sqlx

--- btreemapped/src/json.rs
+++ btreemapped/src/json.rs
@@ -68,3 +68,79 @@ where
 
     postgres_types::to_sql_checked!();
 }
+
+#[cfg(feature = "sqlx")]
+impl<'r, T> sqlx::Decode<'r, sqlx::Postgres> for PgJson<T>
+where
+    T: serde::de::DeserializeOwned,
+{
+    fn decode(
+        value: sqlx::postgres::PgValueRef<'r>,
+    ) -> Result<PgJson<T>, Box<dyn std::error::Error + 'static + Send + Sync>> {
+        use sqlx::ValueRef;
+
+        if value.is_null() {
+            return Err("tried to decode NULL as PgJson".into());
+        }
+
+        let json_data = match value.format() {
+            sqlx::postgres::PgValueFormat::Binary => {
+                // This indicates that we are reading a JSONB column.
+                let bytes = value.as_bytes()?;
+                if bytes.is_empty() {
+                    return Err("empty binary JSON data".into());
+                }
+
+                // JSONB stores a version byte at the beginning of the string,
+                // which is currently 1.
+                if bytes[0] == 1 {
+                    std::str::from_utf8(&bytes[1..])?
+                } else {
+                    std::str::from_utf8(bytes)?
+                }
+            }
+            sqlx::postgres::PgValueFormat::Text => value.as_str()?,
+        };
+
+        let result: T = serde_json::from_str(json_data)
+            .map_err(|e| format!("JSON deserialization failed: {}", e))?;
+        Ok(PgJson(result))
+    }
+}
+
+#[cfg(feature = "sqlx")]
+impl<T> sqlx::Type<sqlx::Postgres> for PgJson<T> {
+    fn type_info() -> sqlx::postgres::PgTypeInfo {
+        <serde_json::Value as sqlx::Type<sqlx::Postgres>>::type_info()
+    }
+
+    fn compatible(ty: &sqlx::postgres::PgTypeInfo) -> bool {
+        use sqlx::TypeInfo;
+        *ty == Self::type_info()
+            || ty.name() == "JSON"
+            || ty.name() == "JSONB"
+            || ty.name() == "json"
+            || ty.name() == "jsonb"
+    }
+}
+
+#[cfg(feature = "sqlx")]
+impl<'q, T> sqlx::Encode<'q, sqlx::Postgres> for PgJson<T>
+where
+    T: serde::Serialize,
+{
+    fn encode_by_ref(
+        &self,
+        buf: &mut sqlx::postgres::PgArgumentBuffer,
+    ) -> Result<sqlx::encode::IsNull, Box<dyn std::error::Error + Send + Sync + 'static>>
+    {
+        match serde_json::to_value(&self.0) {
+            Ok(json_value) => {
+                <serde_json::Value as sqlx::Encode<sqlx::Postgres>>::encode(
+                    json_value, buf,
+                )
+            }
+            Err(_) => Err(Box::new(sqlx::Error::Encode("null value".into()))),
+        }
+    }
+}
